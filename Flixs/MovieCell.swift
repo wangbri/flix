f1bad461c2b49a3b8699d220dfb5c56392fcb0ca
Brian Wang <wangbri@Brians-Mac.local>
2016-01-25 20:24:13 -0600

    initial commit

---
+++ Flixs/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flixs
+//
+//  Created by Brian Wang on 1/25/16.
+//  Copyright © 2016 wangco. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
